9ed03d68759bbc542f074e0962043cb3d733493c
Ruby Chan :3 <elangpk13@gmail.com>
2025-03-15 00:24:57 +0700

    Update setname.js

--- commands/setname.js
+++ commands/setname.js
@@ -1,13 +1,12 @@
 module.exports = {
     name: 'setname',
-    execute: async (client, msg, args, owner, config, saveConfig) => {
+    execute: async (sock, msg, args, owner, config, saveConfig) => {
         if (!args.length) return msg.reply('Provide a name! Example: !setname Ruby {date}');
 
         let name = args.join(' ').replace('{date}', new Date().toISOString().split('T')[0]);
         config.nameHistory.unshift(name);
         if (config.nameHistory.length > 5) config.nameHistory.pop();
-        await client.updateProfileName(name);
         await saveConfig();
-        await msg.reply(`Name updated: ${name}\nHistory: ${config.nameHistory.join(', ')}`);
+        await msg.reply(`Name update not supported directly. Recorded: ${name}\nHistory: ${config.nameHistory.join(', ')}`);
     }
 };
