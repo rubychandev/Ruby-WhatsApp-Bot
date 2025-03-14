37b3664d8457a83bc94b43ef9f4fb77f13e34d2e
Ruby Chan :3 <elangpk13@gmail.com>
2025-03-15 00:22:46 +0700

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -1,10 +1,16 @@
 module.exports = {
     name: 'owner',
-    execute: async (client, msg, args, owner, config, whisperLimits, saveWhisperLimits) => {
+    execute: async (sock, msg, args, owner, config, whisperLimits, saveWhisperLimits) => {
         const senderNumber = (await msg.getContact()).id.user;
-        const isPrivileged = senderNumber === owner || (config.admins.some(a => a.number === senderNumber) && msg.isGroup && (await msg.getChat()).participants.some(p => p.id._serialized === senderNumber + '@c.us' && p.isAdmin));
+        const isPrivileged = senderNumber === owner || (config.admins.some(a => a.number === senderNumber) && msg.isGroup && (await msg.getChat()).participants.some(p => p.id._serialized.split('@')[0] === senderNumber && p.isAdmin));
         const currentDate = new Date().toISOString().split('T')[0];
         whisperLimits[senderNumber] = whisperLimits[senderNumber] || { date: currentDate, whisperCount: 0, ownerCount: 0, meCount: 0 };
+
+        if (args[0] === 'message' && isPrivileged) {
+            config.ownerMessage = args.slice(1).join(' ');
+            await saveConfig();
+            return msg.reply(`Owner message set to: ${config.ownerMessage}`);
+        }
 
         if (!isPrivileged) {
             if (whisperLimits[senderNumber].date !== currentDate) whisperLimits[senderNumber] = { date: currentDate, whisperCount: 0, ownerCount: 0, meCount: 0 };
@@ -13,9 +19,9 @@ module.exports = {
             await saveWhisperLimits();
         }
 
-        const ownerContact = await client.getContactById(`${owner}@c.us`);
-        const status = ownerContact.status || 'Unknown';
+        const presence = msg.ownerPresence ? msg.ownerPresence() : 'unknown';
         const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:Bot Owner\nTEL;TYPE=CELL:${owner}\nEND:VCARD`;
-        await msg.reply(`Here is the bot owner's contact (Status: ${status}):`, null, { contacts: [{ vcard }] });
+        const message = config.ownerMessage ? `\nMessage: ${config.ownerMessage}` : '';
+        await msg.reply(`Here is the bot owner's contact (Status: ${presence})${message}:`, null, { contacts: [{ vcard }] });
     }
 };
