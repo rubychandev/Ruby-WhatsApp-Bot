14f0706d9a609c17131849adc277bd0596d4644f
Ruby Chan :3 <elangpk13@gmail.com>
2025-03-15 00:23:11 +0700

    Update me.js

--- commands/me.js
+++ commands/me.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: 'me',
-    execute: async (client, msg, args, owner, config, whisperLimits, saveWhisperLimits) => {
+    execute: async (sock, msg, args, owner, config, whisperLimits, saveWhisperLimits) => {
         const sender = await msg.getContact();
         const senderNumber = sender.id.user;
-        const isPrivileged = senderNumber === owner || (config.admins.some(a => a.number === senderNumber) && msg.isGroup && (await msg.getChat()).participants.some(p => p.id._serialized === senderNumber + '@c.us' && p.isAdmin));
+        const isPrivileged = senderNumber === owner || (config.admins.some(a => a.number === senderNumber) && msg.isGroup && (await msg.getChat()).participants.some(p => p.id._serialized.split('@')[0] === senderNumber && p.isAdmin));
         const currentDate = new Date().toISOString().split('T')[0];
         whisperLimits[senderNumber] = whisperLimits[senderNumber] || { date: currentDate, whisperCount: 0, ownerCount: 0, meCount: 0 };
 
@@ -14,7 +14,7 @@ module.exports = {
             await saveWhisperLimits();
         }
 
-        const isGroupAdmin = msg.isGroup && (await msg.getChat()).participants.some(p => p.id._serialized === senderNumber + '@c.us' && p.isAdmin);
+        const isGroupAdmin = msg.isGroup && (await msg.getChat()).participants.some(p => p.id._serialized.split('@')[0] === senderNumber && p.isAdmin);
         const customName = args.join(' ') || sender.pushname || 'Unknown';
         const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:${customName}${msg.isGroup ? ` (${isGroupAdmin ? 'Admin' : 'Member'})` : ''}\nTEL;TYPE=CELL:${senderNumber}\nEND:VCARD`;
         await msg.reply('Here is your contact:', null, { contacts: [{ vcard }] });
