1247a4b0ba45b881f14f008b99afd11f9b97bc1d
Ruby Chan :3 <elangpk13@gmail.com>
2025-03-15 00:24:30 +0700

    Update setbio.js

--- commands/setbio.js
+++ commands/setbio.js
@@ -1,13 +1,12 @@
 module.exports = {
     name: 'setbio',
-    execute: async (client, msg, args, owner, config, saveConfig) => {
+    execute: async (sock, msg, args, owner, config, saveConfig) => {
         if (!args.length) return msg.reply('Provide a bio text! Example: !setbio Bot since {date}');
 
         let bio = args.join(' ').replace('{date}', new Date().toISOString().split('T')[0]);
         config.bioHistory.unshift(bio);
         if (config.bioHistory.length > 5) config.bioHistory.pop();
-        await client.setStatus(bio);
         await saveConfig();
-        await msg.reply(`Bio updated: ${bio}\nHistory: ${config.bioHistory.join(', ')}`);
+        await msg.reply(`Bio update not supported directly. Recorded: ${bio}\nHistory: ${config.bioHistory.join(', ')}`);
     }
 };
